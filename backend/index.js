a60ad5192875623cbde069573dfe0ead19195a09
WellingstonRoberti <32345901+WellingstonRoberti@users.noreply.github.com>
2020-03-24 10:41:49 -0300

    Metodos HTTP e Tipos de Parametros

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,29 @@ const express = require('express');
 
 const app = express();
 
-app.get ('/', (request, response) => {
+app.use(express.json());
+
+/*
+Metodos HTTP:
+
+    GET: busca informação
+    POST: cria informação
+    PUT: altera informação
+    delete: deleta informação
+*/
+
+/*  
+Tipos de Parametros:
+
+    Query Params: paramentros nomeados enviados na rota apos "?" ( Filtros, paginação,..)
+    Route Params: Parametros para identificar recursos
+    Request Body: Corpo da requisição, utilizado para criar ou alterar recursos.
+*/
+
+app.psot ('/users', (request, response) => {
+    const body = request.body;
+
+    console.log(body);
     //return response.send('Hello World');
     return response.json({
         evento: 'Hello World',
